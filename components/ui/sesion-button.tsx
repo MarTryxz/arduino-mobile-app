d56f4f9e286805142581105f47fe3cec7ea4858c
Martin Romo <martin.romoarros224@gmail.com>
2025-07-04 03:26:01 -0400

    Add mobile sidebar navigation and improve button styles in authentication components

--- components/ui/sesion-button.tsx
+++ components/ui/sesion-button.tsx
@@ -5,8 +5,7 @@ import { dark } from '@clerk/themes'
 export function SignInPersonalizado() {
   return (
     <SignInButton mode="modal" appearance={{baseTheme: dark}}>
-      <button style={{ minWidth: 120, position: 'relative', cursor: 'pointer', padding: '12px 17px', border: 0, borderRadius: 7, boxShadow: 'inset 0 0 0 1px rgba(255, 255, 255, 0.1)', background: 'radial-gradient(ellipse at bottom, rgba(71, 81, 92, 1) 0%, rgba(11, 21, 30, 1) 45%)', color: 'rgb(255, 255, 255, 0.66)', transition: 'all 1s cubic-bezier(0.15, 0.83, 0.66, 1)',
-}}>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
 Iniciar sesion</button>
     </SignInButton>
   )
@@ -15,8 +14,7 @@ Iniciar sesion</button>
 export function SignUpPersonalizado() {
   return (
     <SignUpButton mode="modal" appearance={{baseTheme: dark}}>
-      <button style={{ minWidth: 120, position: 'relative', cursor: 'pointer', padding: '12px 17px', border: 0, borderRadius: 7, boxShadow: 'inset 0 0 0 1px rgba(255, 255, 255, 0.1)', background: 'radial-gradient(ellipse at bottom, rgba(71, 81, 92, 1) 0%, rgba(11, 21, 30, 1) 45%)', color: 'rgb(255, 255, 255, 0.66)', transition: 'all 1s cubic-bezier(0.15, 0.83, 0.66, 1)',
-}}>Registrarse</button>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Registrarse</button>
     </SignUpButton>
   )
 }
