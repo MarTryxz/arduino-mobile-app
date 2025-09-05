0da499fb3865015a619dcb810fef98878246bd54
Martin Romo <martin.romoarros224@gmail.com>
2025-09-05 00:11:24 -0400

    Añadir icono de aplicación y archivos de imagen WEBP para mejorar el rendimiento

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <head>
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#007bff" />
+          <link rel="icon" href="/openrakiduamlogo.webp" type="image/webp" />
         </head>
         <body>
           {children}
